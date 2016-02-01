44188301a63072545881d5a7ed936c60923bc964
l19g2004 <lukas.gesing@uni-bonn.de>
2016-02-01 13:48:23 +0100

    added some parts of the dancegame folder

---
+++ Source/MyProjects/24_Urho2DSprite/import.h
@@ -0,0 +1,16 @@
+#ifndef IMPORT_H
+#define IMPORT_H
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+//#include <sound.h>
+#include <Arrow.h>
+
+bool checkFileExists(const std::string& name);
+void split(const std::string& s, char c, std::vector<std::string>& v);
+std::vector<Arrow> readFile(std::string dfilename);
+#endif
